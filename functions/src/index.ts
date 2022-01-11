bafbf633cf2c8b9b19858ffdd1d895ea2f029f52
Olohundare Nasirudeen <iamnasirudeen@gmail.com>
2022-01-11 16:34:47 +0100

    Firebase Integration

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from "firebase-functions";
+import config from "./config";
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {
+    tructuredData: true,
+  });
+  response.send("Hello from Firebase1! \n" + JSON.stringify(config));
+});
